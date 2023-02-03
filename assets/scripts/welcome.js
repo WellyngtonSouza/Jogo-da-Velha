ccff53ebe8345ff91e7e8b23579e06589df34078
wellyngton souza <wellyngtonmonteiro84@gmail.com>
2023-02-03 12:49:55 -0300

    update

--- assets/scripts/welcome.js
+++ assets/scripts/welcome.js
@@ -10,7 +10,7 @@ function cli(element) {
         valor.push(input1.value)
         valor.push(input2.value)
         localStorage.setItem("valor", JSON.stringify(valor))
-      element.setAttribute("href", "../game/jogo.html")
+        element.href = "../game/jogo.html"
     }
 }
 
